c997a8bb8d09319b602fdeece4647b8e9bf20d69
unfelix <shevko.evheniy@gmail.com>
2020-10-05 21:06:39 +0300

    add drawShip

--- main.js
+++ main.js
@@ -4,3 +4,24 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight - 4;
 
+canvas.onclick = (e) => {
+  coords.push(e.layerX, e.layerY);
+};
+
+document.body.onkeydown = (e) => {
+  if (e.key == "Enter") {
+    drawShip(...coords);
+    coords.length = 0;
+  }
+};
+
+const coords = [];
+
+function drawShip(...coords) {
+  ctx.beginPath();
+  for (let i = 0; i < coords.length; i += 2) {
+    ctx.lineTo(coords[i], coords[i + 1]);
+  }
+  ctx.stroke();
+  ctx.closePath();
+}
