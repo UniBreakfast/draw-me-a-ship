9459d5b3e92e14f929a1a71c8e670507db18a03e
unfelix <shevko.evheniy@gmail.com>
2020-10-07 18:32:07 +0300

    drawShip change to drawContour

--- main.js
+++ main.js
@@ -5,22 +5,22 @@ canvas.width = innerWidth;
 canvas.height = innerHeight - 4;
 
 canvas.onclick = (e) => {
-  coords.push(e.layerX, e.layerY);
+  coords.push({ x: e.layerX, y: e.layerY });
 };
 
 document.body.onkeydown = (e) => {
   if (e.key == "Enter") {
-    drawShip(...coords);
+    drawContour(...coords);
     coords.length = 0;
   }
 };
 
 const coords = [];
 
-function drawShip(...coords) {
+function drawContour(...coords) {
   ctx.beginPath();
-  for (let i = 0; i < coords.length; i += 2) {
-    ctx.lineTo(coords[i], coords[i + 1]);
+  for (let i = 0; i < coords.length; i++) {
+    ctx.lineTo(coords[i].x, coords[i].y);
   }
   ctx.stroke();
   ctx.closePath();
