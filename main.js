775a209c3631e57442a1e5c7e02ab2dcb30f2675
unfelix <shevko.evheniy@gmail.com>
2020-10-07 19:34:02 +0300

    add fillPalette &  palette layout

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const coords = [];
+const colors = [
+  "black",
+  "yellow",
+  "blue",
+  "grey",
+  "white",
+  "orange",
+  "pink",
+  "green",
+  "black",
+  "yellow",
+  "blue",
+  "grey",
+  "white",
+  "orange",
+  "pink",
+  "green",
+];
 
 canvas.width = innerWidth;
 canvas.height = innerHeight - 4;
+
+fillPalette();
 
 canvas.onclick = (e) => {
   coords.push({ x: e.layerX, y: e.layerY });
@@ -15,7 +36,7 @@ document.body.onkeydown = (e) => {
   }
 };
 
-const coords = [];
+//function declarations
 
 function drawContour(...coords) {
   ctx.beginPath();
@@ -24,4 +45,11 @@ function drawContour(...coords) {
   }
   ctx.stroke();
   ctx.closePath();
+}
+
+function fillPalette() {
+  const colorDivs = document.querySelectorAll("div.color");
+  for (let i = 0; i <= colors.length; i++) {
+    colorDivs[i].style.backgroundColor = colors[i];
+  }
 }
